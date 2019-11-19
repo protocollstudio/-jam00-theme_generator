1fb43d1e25880c7073bf23adbf7d801f8756b57b
marccharton <kramoogle@gmail.com>
2019-11-19 14:44:30 +0100

    feat: add async use for getting words

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
-import * as generator from "./generator.js"
+import * as generator from "./generator.js";
 
-window.addEventListener('DOMContentLoaded', function () {
-    changeTheme()
-})
+window.addEventListener("DOMContentLoaded", function() {
+  changeTheme();
+});
 
 async function changeTheme() {
-    
-    let themeContainer = document.getElementById("theme-container")
-    let paragraph = document.createElement('p')
-    let theme = await generator.newTheme()
-    
-    paragraph.innerHTML =  `<h2> ${theme.verb} ${theme.adjective} ${theme.noun} </h2>`
-    themeContainer.appendChild(paragraph)
+  let themeContainer = document.getElementById("theme-container");
+  let paragraph = document.createElement("p");
+  let theme = await generator.newTheme();
 
+  paragraph.innerHTML = `<h2> ${theme.verb} ${theme.adjective} ${theme.noun} </h2>`;
+  themeContainer.appendChild(paragraph);
 }
