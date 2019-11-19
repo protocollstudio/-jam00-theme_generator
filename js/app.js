8efdd28f9da2f58de10ae039312bc9b83bfd181e
marccharton <kramoogle@gmail.com>
2019-11-19 15:56:07 +0100

    feat: add animation on regeneration

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 import * as generator from './generator.js';
 
 window.addEventListener('DOMContentLoaded', function() {
-  const button = document.getElementById("generate-button");
-  button.addEventListener("click", changeTheme);
+	const button = document.getElementById('generate-button');
+	button.addEventListener('click', () => {
+      setTimeout(() => {
+        changeTheme();
+      }, 300);
+      hideContainer();
+  });
 });
 
 async function changeTheme() {
-  const theme = await generator.newTheme();
+	const theme = await generator.newTheme();
 	updateTheme(theme);
 }
 
@@ -24,13 +29,27 @@ function updateTheme(theme) {
 			idName: 'noun',
 			word: theme.noun
 		}
-  ].forEach(mappingWordTag => {
-    changeTagWord(mappingWordTag);
+	].forEach((mappingWordTag) => {
+		changeTagWord(mappingWordTag);
   });
+  
+  showContainer();
 }
 
 function changeTagWord(mappingWordTag) {
-  const tag = document.getElementById(mappingWordTag.idName);
-  const underlinedTag = "<span class='underline'></span>";
-  tag.innerHTML = `${mappingWordTag.word} ${underlinedTag}`;
+	const tag = document.getElementById(mappingWordTag.idName);
+	const underlinedTag = "<span class='underline'></span>";
+	tag.innerHTML = `${mappingWordTag.word} ${underlinedTag}`;
+}
+
+function hideContainer() {
+  const container = document.getElementById("theme-container");
+  container.classList.remove("visible");
+  container.classList.add("hide");iner.classList.add("visible");
+}
+
+function showContainer() {
+  const container = document.getElementById("theme-container");
+  container.classList.remove("hide");
+  container.classList.add("visible");
 }
